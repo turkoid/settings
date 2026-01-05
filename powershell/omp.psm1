5cf1f1e7d93ce92c484ec3ea0dc75b5ff23bae35
John Ross <jlross42@gmail.com>
2026-01-05 02:20:19 -0500

    dont break modules if repo is moved

--- powershell/omp.psm1
+++ powershell/omp.psm1
@@ -1,7 +1,9 @@
+$script:OhMyPoshDir = Join-Path "$PSScriptRoot" "..\oh-my-posh"
+
 class CustomOhMyPoshThemes : System.Management.Automation.IValidateSetValuesGenerator {
     [String[]] GetValidValues() {
         $ValidProfiles = [System.Collections.ArrayList]::new()
-        foreach ($Path in (Get-ChildItem (Join-Path "$env:dev_home" 'git\settings\oh-my-posh\*.omp.json'))) {
+        foreach ($Path in (Get-ChildItem (Join-Path "$script:OhMyPoshDir" "*.omp.json"))) {
             $Name = $Path.Name.Substring(0, $Path.Name.Length - 9)
             $ValidProfiles.Add($Name)
             if ($Name -like 'turkoid*') {
@@ -23,10 +25,10 @@ function Set-CustomPoshPrompt() {
         [string]
         $Name = 'turkoid'
     )
-    if ($Name -like 'tk*' -and !(Test-Path (Join-Path $env:dev_home "git\settings\oh-my-posh\$Name.omp.json"))) {
+    if ($Name -like 'tk*' -and !(Test-Path (Join-Path "$script:OhMyPoshDir" "$Name.omp.json"))) {
         $Name = 'turkoid' + $Name.Substring(2)
     }
-    oh-my-posh init pwsh --config (Join-Path $env:dev_home "git\settings\oh-my-posh\$Name.omp.json") | Invoke-Expression
+    oh-my-posh init pwsh --config (Join-Path "$script:OhMyPoshDir" "$Name.omp.json") | Invoke-Expression
 }
 
 Set-Alias omp 'Set-CustomPoshPrompt'
--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -1,8 +1,9 @@
-Import-Module $env:dev_home\git\settings\powershell\common
-Import-Module $env:dev_home\git\settings\powershell\cmd-emulation
-Import-Module $env:dev_home\git\settings\powershell\bash-emulation
-Import-Module $env:dev_home\git\settings\powershell\virtualenvwrapper
-Import-Module $env:dev_home\git\settings\powershell\omp
+Import-Module $PSScriptRoot\common
+Import-Module $PSScriptRoot\cmd-emulation
+Import-Module $PSScriptRoot\bash-emulation
+Import-Module $PSScriptRoot\virtualenvwrapper
+Import-Module $PSScriptRoot\omp
+Import-Module git-completion
 
 Set-CustomPoshPrompt
 Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
